3707fc618fb36b2057f4368f957f9efd5a7d51d9
marcin.jagaczewski@gmail.com <marcin.jagaczewski@gmail.com>
2025-08-14 15:24:12 +0200

    program is split on files

--- inc/gpio.hpp
+++ inc/gpio.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+#include <functional>
+
 namespace gpio
 {
+    
+    void isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
 
+    class GPIO
+    {
+        public:
+            GPIO();
+            GPIO(gpio_callback_handler_t isr_handler);
+            GPIO* getAddress();
+            void setLed(bool state);
+    };
 };
-
-
-
-
-
-
-
-void gpioInit();
